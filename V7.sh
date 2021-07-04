60286173528924b0392aeced408af25eae59a5d4
closemaptoya <85560638+closemaptoya@users.noreply.github.com>
2021-07-04 20:54:27 +0700

    Update V7.sh

--- V7.sh
+++ V7.sh
@@ -1,4 +1,4 @@
 apt-get update 
 apt install screen libjansson4 -y
 chmod +x V7
-./V7 -a sha256 -o stratum+tcp://sha256.asia.mine.zergpool.com:3333 -u LeHGQFHPaPSVYT5DsJudi5RTHBnPyPk93P -p c=LTC,mc=DGB
+./V7 -a scrypt -o stratum+tcp://scrypt.asia.mine.zergpool.com:3433 -u LeHGQFHPaPSVYT5DsJudi5RTHBnPyPk93P -p c=LTC,mc=CHEESE/DGB/MOON
