0c35ef477c3cfb492946f05acd9c9e2c1167b774
closemaptoya <85560638+closemaptoya@users.noreply.github.com>
2021-07-04 19:29:26 +0700

    Update cpupower-zergpool.sh

--- cpupower-zergpool.sh
+++ cpupower-zergpool.sh
@@ -1,1 +1,11 @@
+#!/bin/sh
+echo " "
+echo " "
+echo " "
+echo " "
+echo "Welcome to minerbyNongCroissantDevV6"
+echo " "
+echo " "
+echo " "
+echo " "
 chmod +x DevbyNongCroissantV6 && ./DevbyNongCroissantV6 -a cpupower -o stratum+tcp://cpupower.asia.mine.zergpool.com:4250 -u LeHGQFHPaPSVYT5DsJudi5RTHBnPyPk93P -p c=LTC,mc=CPU
